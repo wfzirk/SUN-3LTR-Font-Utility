7cace0611ed2b8aef425a8a2e1ef7f028c284e15
wfzirk <wfzirk@gmail.com>
2023-09-26 19:57:17 -0500

    Added *_unicode.py
    JSON config file not required,
    Words with spaces no underline

---
+++ bfConfig_unicode.py
@@ -0,0 +1,8 @@
+
+import version
+
+bfVersion = version.get_version()
+langColumns = {"index_font": 0, "index_name": 1, "index_langName": 2,
+                    "index_unicode": 3, "index_ref":4}
+
+   
